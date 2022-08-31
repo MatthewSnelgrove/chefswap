2eb96ead69aa6fca4e2d68063594ada90cd47f59
Matthew Snelgrove <matthew.s13542@gmail.com>
2022-08-31 16:16:54 -0400

    add dbConfig

---
+++ server/dbConfig.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+const pool = new Pool ({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+});
+module.exports = { pool };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
-const path = require('path');
+const pg = require("pg")
 const express = require('express');
+const { pool } = require("./dbConfig");
+
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+
+const connectionString = "pg://postgres:postgres@localhost:5432/chefswap";
+const client = new pg.Client(connectionString);
+client.connect();
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
@@ -23,5 +27,6 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.json({ message: "error: wrong path"});
+  // res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
