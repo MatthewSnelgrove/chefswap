43c2b5c4a5a67b3a9f3ae05982b6b38cd52295ad
Andre Fong <fongandre123@gmail.com>
2022-08-31 18:49:20 -0400

    Add routing for skeleton pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
-import logo from "./logo.svg";
+// import ReactDOM from 'react-dom';
 import "./App.css";
+import AboutPage from "./components/AboutPage";
+import AboutUsPage from "./components/AboutUsPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+import MySwapsPage from "./components/MySwapsPage";
+import MyMessagesPage from "./components/MyMessagesPage"
+import FindSwapPage from "./components/FindSwapPage";
+import UserInfoPage from "./components/UserInfoPage";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("localhost:3001/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AboutPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+
+        {/* May not need */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/my-swaps" element={<MySwapsPage />} />
+        <Route path="/my-messages" element={<MyMessagesPage />} />
+        <Route path="/find-swap" element={<FindSwapPage />} />
+        <Route path="/user/somekey" element={<UserInfoPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
