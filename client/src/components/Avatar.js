699e9930e2c8ec165fe4502e44397d58ebcf3e10
Andre Fong <fongandre123@gmail.com>
2022-09-04 21:06:29 -0400

    Add Avatar component

---
+++ client/src/components/Avatar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import "./Avatar.scss";
+
+/**
+ * Takes in user data to display user profile picture
+ */
+export default class Avatar extends Component {
+  render() {
+    const pfpURL = this.props.avatarURL;
+    const size = this.props.size;
+    const style = {
+      width: `${size}px`,
+      height: `${size}px`,
+      objectFit: "cover",
+    };
+
+    return (
+      <div className="avatar-container">
+        <img src={pfpURL} alt="Avatar" className="avatar" style={style} />
+      </div>
+    )
+  }
+}
+
+Avatar.propTypes = {
+  avatarURL: PropTypes.string,
+  size: PropTypes.number,
+};
+
+Avatar.defaultProps = {
+  avatarURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png",
+  size: 30,
+};
