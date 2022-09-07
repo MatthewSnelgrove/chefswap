50839977819900487a13ce333b45687b1b4ae3f6
Andre Fong <99469779+andre-fong@users.noreply.github.com>
2022-09-07 19:35:19 -0400

    Add signup and login skeleton pages (#7)

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./_SignupLogin.scss";
 
 function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Prevented form submit.");
+  }
+
   return (
-    <div>Login</div>
+    // <form action="/api/auth/login" method="post" className="signup-login-form">
+    <form onSubmit={handleSubmit} className="signup-login-form">
+      <fieldset>
+        <legend>Login</legend>
+
+        <input type="text" id="username" name="username" placeholder="*Username" required />
+        <input type="password" id="password" name="password" placeholder="*Password" minLength={3} required />
+      </fieldset>
+
+      <button type="submit" className="submit-btn">Submit</button>
+    </form>
   )
 }
 
