9adc7d7ccb671287b25c04cda99d10bdc3881192
Andre Fong <fongandre123@gmail.com>
2022-10-15 12:59:05 -0400

    Updates

--- server/utils/addressToGmapsUrl.js
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-export default function (address) {
-  const { address1, address2, address3, city, province } = address;
-  const addressString = `${address1} ${address2} ${address3}, ${city}, ${province}`;
-  addressString.replace(/\s+/g, "+");
-  return `https://maps.googleapis.com/maps/api/geocode/json?address=${addressString}&key=${process.env.GMAPS_KEY}`;
-}
