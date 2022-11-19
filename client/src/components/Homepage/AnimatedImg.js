bc96b947aca3f2adeab8d8a2aef825a427bb8ae2
Andre Fong <fongandre123@gmail.com>
2022-11-18 23:19:37 -0500

    Add fade in and up animation to graphics

--- client/src/components/Homepage/AnimatedImg.js
+++ client/src/components/Homepage/AnimatedImg.js
@@ -10,19 +10,37 @@ import "./styles/AnimatedImg.scss";
  * @param {Array} position Absolute position from reference image [<left>, <bottom>] (in %)
  * @param {Number} delay Animation delay in seconds
  * @param {Number} rotate Degrees rotated
+ * @param {Number} section Section #
  */
-function AnimatedImg({ src, alt, size, position, delay = 0, rotate = 0, children }) {
+function AnimatedImg({ REF, src, alt, size, position = [0, 0], delay = 0, rotate = 0, section, children }) {
 
   // If no position (position of image is reference)
-  if (!position) {
+  if (REF) {
+    let style = {
+      transitionDelay: `${delay}s`,
+    };
+
+    try {
+      style.left = `${position[0]}%`;
+      style.bottom = `${position[1]}%`;
+    } catch (error) {
+      console.log(error);
+
+      style.left = 0;
+      style.bottom = 0;
+    }
 
     return (
       <div className="AnimatedImgContainer"
-        style={{ width: `${size}%`, transform: `rotate(${rotate}deg)`, animationDelay: `${delay}s` }}>
+        style={{
+          width: `${size}%`,
+          transform: `rotate(${rotate}deg)`,
+        }}>
         <img
           src={src}
           alt={alt}
-          className="AnimatedImg" />
+          className={`AnimatedImg section${section}`}
+          style={style} />
 
         {children}
 
@@ -36,7 +54,7 @@ function AnimatedImg({ src, alt, size, position, delay = 0, rotate = 0, children
       width: `${size}%`,
       position: "absolute",
       transform: `rotate(${rotate}deg)`,
-      animationDelay: `${delay}s`,
+      transitionDelay: `${delay}s`,
     };
 
     try {
@@ -53,7 +71,7 @@ function AnimatedImg({ src, alt, size, position, delay = 0, rotate = 0, children
       <img
         src={src}
         alt={alt}
-        className="AnimatedImg"
+        className={`AnimatedImg section${section}`}
         style={style} />
     );
   }
--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Typewriter from "../components/Homepage/Typewriter";
 import StyledBtn from "../components/Homepage/StyledBtn";
 import AnimatedImg from "../components/Homepage/AnimatedImg";
@@ -8,26 +8,59 @@ import "./styles/AboutPage.scss";
  * Page at "/"
  */
 function AboutPage() {
+  const [section, setSection] = useState(1);
+
+  const animationTimes = {
+    pan: 0.2,
+    onions: 0.55,
+    potato: 0.58,
+    carrot: 0.61,
+    broccoli: 0.64,
+  };
 
   function handleScrollY() {
-    // Animate intro section
+    console.log(`Section = ${section}`);    // test
+    // Animate intro section (1)
     if (window.scrollY < window.innerHeight / 2) {
-      console.log("On first page");
+      if (section !== 1) {
+        setSection(1);
+        return 1;
+      }
     }
-
-    // Animate demo section
+    // Animate demo section (2)
     else if (window.scrollY < window.innerHeight * 3 / 2) {
-      console.log("On second page");
+      if (section !== 2) {
+        setSection(2);
+        return 2;
+      }
     }
   }
 
   useEffect(() => {
     window.onscroll = handleScrollY;
 
+    if (section === 1) {
+      animateSection(1, 10);
+    }
+
+    if (section === 2) {
+      animateSection(1, -10);
+    }
+
     return () => {
       window.onscroll = null;
     };
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [section]);
+
+  // Animate section num in or out of frame
+  function animateSection(num, up = 0) {
+    let imgs = document.querySelectorAll(`.section${num}`);
+    imgs.forEach(img => {
+      img.style.bottom = `${parseInt(img.style.bottom) + up}%`;
+      img.style.opacity = (up >= 0) ? 1 : 0;
+    });
+  }
 
   return (
     <>
@@ -38,7 +71,7 @@ function AboutPage() {
             with aspiring <br></br> chefs of any level.
           </h1>
           <div className="about-blurb">
-            Chefswap helps users across Canada connect with <br></br> each other — through cooking!
+            Chefswap helps users across Canada connect with each other through a love for cooking and eating!
           </div>
           <StyledBtn arrowed text="START SWAPPING" link="/signup" />
         </main>
@@ -49,38 +82,44 @@ function AboutPage() {
           paddingTop: "10%",
         }}>
           <AnimatedImg
+            REF
             src={process.env.PUBLIC_URL + "/pan-graphic.png"}
             alt="Frying pan tossing ingredients"
             size={80}
             rotate={-5}
-            delay={0.5}>
+            delay={animationTimes.pan}
+            section={1}>
             <AnimatedImg
               src={process.env.PUBLIC_URL + "/broccoli.png"}
               alt="Broccoli"
-              position={[50, 95]}
+              position={[50, 85]}
               size={15}
-              delay={0.55} />
+              delay={animationTimes.broccoli}
+              section={1} />
             <AnimatedImg
               src={process.env.PUBLIC_URL + "/carrot.png"}
               alt="Carrot"
-              position={[37, 140]}
+              position={[37, 130]}
               size={20}
               rotate={20}
-              delay={0.55} />
+              delay={animationTimes.carrot}
+              section={1} />
             <AnimatedImg
               src={process.env.PUBLIC_URL + "/onions.png"}
               alt="Onions"
-              position={[5, 110]}
+              position={[5, 100]}
               size={25}
               rotate={-20}
-              delay={0.55} />
+              delay={animationTimes.onions}
+              section={1} />
             <AnimatedImg
               src={process.env.PUBLIC_URL + "/potato.png"}
               alt="Potato"
-              position={[22, 53]}
+              position={[22, 43]}
               size={18}
               rotate={130}
-              delay={0.55} />
+              delay={animationTimes.potato}
+              section={1} />
           </AnimatedImg>
         </div>
 
