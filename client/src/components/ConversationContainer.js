c44926136ac2a0155151bac28a81bae64b37267e
zayleak <victorghurst@gmail.com>
2023-04-29 14:29:21 -0400

    add connect socket code for testing

--- client/src/components/ConversationContainer.js
+++ client/src/components/ConversationContainer.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import PeopleContainer from "./PeopleContainer";
 import "./styles/ConversationContainer.scss";
 import Chatbox from "./Chatbox";
-
+const { io } = require("socket.io-client");
 /**
  * Container component for entire conversations page (/my-messages)
  * @use PeopleContainer, Chatbox
  */
 export default class ConversationContainer extends Component {
   render() {
+    const socket = io(global.config.server)
+
+    socket.on("connect", () => {
+      console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    });
+    
+
     return (
       <div className="conversation-flex">
         <div className="conversation-container">
