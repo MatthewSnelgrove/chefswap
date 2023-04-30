ea7ffecf87df6211f7a75fe11e1773a3dcf08256
Andre Fong <fongandre123@gmail.com>
2023-04-30 19:39:20 -0400

    chore: Delete unneeded files for migration

--- client/src/App.js
+++ client/src/App.js
@@ -11,22 +11,20 @@ import FindSwapPage from "./pages/FindSwapPage";
 import UserInfoPage from "./pages/UserInfoPage";
 import Navbar from "./components/Navbar";
 import UserEditPage from "./pages/UserEditPage";
-import EditProfile from "./components/EditProfile"
-import EditPassword from './components/EditPassword'
-import EditGallery from './components/EditGallery'
-import EditPersonal from './components/EditPersonal'
+import EditProfile from "./components/EditProfile";
+import EditPassword from "./components/EditPassword";
+import EditGallery from "./components/EditGallery";
+import EditPersonal from "./components/EditPersonal";
 import FindUserPage from "./pages/FindUserPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify"
+import { ToastContainer } from "react-toastify";
 import OnlyLoggedOut from "./components/OnlyLoggedOut";
 
 const queryClient = new QueryClient();
 
-
-
+// For marking which pages are left to implement
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -34,22 +32,45 @@ function App() {
         <Navbar />
         <ToastContainer autoClose={2000} />
         <Routes>
-          <Route path="/" element={<AboutPage />} />
+          {/* <Route path="/" element={<AboutPage />} /> */}
           <Route path="/about" element={<AboutMorePage />} />
 
           {/* May not need */}
-                
-          <Route path="/my-swaps" element={<MySwapsPage />} />
-          <Route path="/my-messages" element={<MyMessagesPage />} />
-          <Route path="/find-swap" element={<FindSwapPage />} />
-          <Route path="/:id" element={<UserInfoPage />} />
-          <Route path="/accounts/edit" element={<UserEditPage renderType={<EditProfile />} name="EditProfile" />} />
-          <Route path="/accounts/password/change" element={<UserEditPage renderType={<EditPassword />} name="EditPassword" />} />
-          <Route path="/accounts/gallery" element={<UserEditPage renderType={<EditGallery />} name="EditGallery" />} />
-          <Route path="/accounts/personal" element={<UserEditPage renderType={<EditPersonal />} name="EditPersonal" />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />   
-          <Route path="/find-swap/search/:usernameQuery" element={<FindUserPage />} />
+
+          {/* <Route path="/my-swaps" element={<MySwapsPage />} /> */}
+          {/* <Route path="/my-messages" element={<MyMessagesPage />} /> */}
+          {/* <Route path="/find-swap" element={<FindSwapPage />} /> */}
+          {/* <Route path="/:id" element={<UserInfoPage />} /> */}
+          <Route
+            path="/accounts/edit"
+            element={
+              <UserEditPage renderType={<EditProfile />} name="EditProfile" />
+            }
+          />
+          <Route
+            path="/accounts/password/change"
+            element={
+              <UserEditPage renderType={<EditPassword />} name="EditPassword" />
+            }
+          />
+          <Route
+            path="/accounts/gallery"
+            element={
+              <UserEditPage renderType={<EditGallery />} name="EditGallery" />
+            }
+          />
+          <Route
+            path="/accounts/personal"
+            element={
+              <UserEditPage renderType={<EditPersonal />} name="EditPersonal" />
+            }
+          />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<Signup />} />    */}
+          <Route
+            path="/find-swap/search/:usernameQuery"
+            element={<FindUserPage />}
+          />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
