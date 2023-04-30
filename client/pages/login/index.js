067bd9de08c37c4b3c296ba38ba4d739f2559298
Andre Fong <fongandre123@gmail.com>
2023-04-29 20:58:49 -0400

    chore: Remove useEffect

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { fetchLogin } from "../../utils/fetchFunctions";
 import Modal from "../../components/Modal";
 import CredentialField from "../../components/CredentialField2";
