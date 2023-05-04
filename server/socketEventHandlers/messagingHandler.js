aefe6a5902162c9c4f301e58ed745ee8e4ae00b7
Matthew Snelgrove <matthew.s13542@gmail.com>
2023-05-03 20:56:48 -0400

    update message handler to use parser and support errors

--- server/socketEventHandlers/messagingHandler.js
+++ server/socketEventHandlers/messagingHandler.js
@@ -1,16 +1,21 @@
+import { pool } from "../configServices/dbConfig.js";
+import camelize from "camelize";
+import { bucket } from "../configServices/cloudStorageConfig.js";
+import snakeize from "snakeize";
+import { BusinessError, accountNotFound } from "../utils/errors.js";
+import stripNulls from "../utils/stripNulls.js";
+import parseInputMessage from "../socketParsers/inputMessageParser.js";
 export default (io, socket) => {
-  const sendUserMessage = (message, callback) => {
-    //validate message
-    //receiver exists
-    //save message in db
-    console.log("input message: ", message);
-    message.messageUid = "test32e6-c4f8-44d4-9408-cfb4f8499d01";
-    message.senderUid = socket.accountUid;
-    message.receiverUid = "fa4632e6-c4f8-44d4-9408-cfb4f8499d01";
-    message.timestamp = Date.now();
-    console.log("output message: ", message);
-    io.to(message.receiverUid).emit("receiveUserMessage", message);
-    callback(message);
+  const sendMessage = (inputMessage, callback) => {
+    const parsedMessage = parseInputMessage(inputMessage);
+    if (!parsedMessage.errors) {
+      //valid inputMessage
+      parsedMessage.messageUid = "test32e6-c4f8-44d4-9408-cfb4f8499d01";
+      parsedMessage.senderUid = socket.accountUid;
+      parsedMessage.timestamp = Date.now();
+      io.to(parsedMessage.receiverUid).emit("receiveMessage", parsedMessage);
+    }
+    callback(parsedMessage);
   };
 
   const deleteMessage = (messageId) => {
@@ -18,6 +23,6 @@ export default (io, socket) => {
     socket.emit("deleteMessage", messageId);
   };
 
-  socket.on("sendUserMessage", sendUserMessage);
+  socket.on("sendMessage", sendMessage);
   socket.on("deleteMessage", deleteMessage);
 };
