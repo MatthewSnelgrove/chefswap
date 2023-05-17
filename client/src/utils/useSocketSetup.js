c683d26a241f4f4ea2bf47a1b7d95114c3cd6082
MatthewSnelgrove <57278816+MatthewSnelgrove@users.noreply.github.com>
2023-05-17 13:30:34 -0400

    Messaging validation (#100)
    * add conversation to socket schema
    
    * add json validator package
    
    * move socketio listener to useEffect
    
    * update yaml: merge user and system message, assign messages to conversation, add errors
    
    * add disconnection handler
    
    * add socket validation error handler
    
    * add input message parser
    
    * update message handler to use parser and support errors
    
    * update internal error message
    
    * add socket internal error
    
    * throw and catch validation errors
    
    * separate group and dm in socket schema
    
    * add messageId
    
    * install better-ajv-errors, asyncapi/parser, ajv, ajv-keywords
    
    * fix integer format in socketdoc
    
    * add initial socketParser with old method comment
    
    * update error format in socketdoc
    
    * seperate keyword def, validation functions from general setup
    
    * rename parser folder to validator
    
    * add required readOnly "senderUid" property to messages
    
    * use new validator in messagingHandler
    
    * change emit message to sendDmMessage
    
    * do socket validation with socket middleware
    
    * remove socketValidator
    
    * move validateMessageEvent into ajvSetup
    
    * remove validation code from messagingHandler
    
    * rename ajvSetup to socketValidator
    
    * move socketValidator to socketmiddlewares
    
    * move ajvKeywords to utils
    
    * create message hierarchy, make groupUid readOnly
    
    * fix imports, send errors in callback

--- client/src/utils/useSocketSetup.js
+++ client/src/utils/useSocketSetup.js
@@ -3,12 +3,20 @@ import socket from "./socket";
 
 const useSocketSetup = () => {
   useEffect(() => {
+    console.log("connected to socket");
     socket.connect();
     socket.on("connect_error", () => {
       console.log(`connect_error"`);
     });
+    socket.on("receiveUserMessage", (message) => {
+      console.log("received message1", Date.now());
+      console.log(message);
+    });
     return () => {
+      console.log("disconnected from socket");
       socket.off("connect_error");
+      socket.off("receiveUserMessage");
+      socket.disconnect();
     };
   }, []);
 };
---
+++ server/socketMiddlewares/socketValidator.js
@@ -0,0 +1,103 @@
+import { Parser, fromFile } from "@asyncapi/parser";
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+import {
+  addReadOnlyKeyword,
+  addRequiredKeyword,
+} from "../utils/ajvKeywords.js";
+
+const ajvOptions = {
+  allErrors: true,
+  strict: false,
+  unicodeRegExp: false,
+};
+
+const writeAjv = new Ajv(ajvOptions);
+addFormats(writeAjv);
+addRequiredKeyword(writeAjv, "read");
+addReadOnlyKeyword(writeAjv);
+
+const readAjv = new Ajv(ajvOptions);
+addFormats(readAjv);
+addRequiredKeyword(readAjv, "write");
+addReadOnlyKeyword(readAjv);
+
+const parser = new Parser();
+const { document } = await fromFile(parser, "./server/socketdoc.yaml").parse();
+
+const schema = document._json;
+export const payloadValidatorByEvent = new Map();
+export const ackValidatorByEvent = new Map();
+Object.keys(schema.channels).forEach((channelName) => {
+  //publish
+  Object.keys(schema.channels[channelName].publish.message.oneOf).forEach(
+    (messageIndex) => {
+      const message =
+        schema.channels[channelName].publish.message.oneOf[messageIndex];
+      const payloadValidator = writeAjv.compile(message.payload || {});
+      payloadValidatorByEvent.set(message.messageId, payloadValidator);
+      const ackValidator = readAjv.compile(message["x-ack.args"] || {});
+      ackValidatorByEvent.set(message.messageId, ackValidator);
+    }
+  );
+  //subscribe
+  Object.keys(schema.channels[channelName].subscribe.message.oneOf).forEach(
+    (messageIndex) => {
+      const message =
+        schema.channels[channelName].subscribe.message.oneOf[messageIndex];
+      const payloadValidator = readAjv.compile(message.payload || {});
+      payloadValidatorByEvent.set(message.messageId, payloadValidator);
+      const ackValidator = writeAjv.compile(message["x-ack.args"] || {});
+      ackValidatorByEvent.set(message.messageId, ackValidator);
+    }
+  );
+});
+
+function formatSocketErrors(ajvErrors, event) {
+  // for each ajvError, push custom error to errors array
+  const errors = ajvErrors.map((error) => {
+    return {
+      errorType: "validation",
+      event: event,
+      path: error.instancePath,
+      message: `${error.keyword} error`,
+      detail: error.message,
+    };
+  });
+
+  return { errors: errors };
+}
+
+function validateByValidator(validator, instance, next, callback) {
+  const event = validator.schema["x-parser-schema-id"];
+  if (!validator) {
+    const error = {
+      errorType: "business",
+      event: event,
+      message: `event not found`,
+      detail: `event ${event} was not found`,
+    };
+    next([{ errors: [error] }, callback]);
+    return;
+  }
+  const result = validator(instance);
+  if (!result) {
+    const formattedErrors = formatSocketErrors(validator.errors, event);
+    next([formattedErrors, callback]);
+    return;
+  }
+  next();
+}
+
+//for 1. validating user-created payload on publish events
+//AND 2. validating server-created payload on subscribe events
+export function validatePayloadByEvent([event, instance, callback], next) {
+  const validator = payloadValidatorByEvent.get(event);
+  validateByValidator(validator, instance, next, callback);
+}
+//ONLY for validating server-created ack on publish events
+//user-created acks on subscribe events are not validated or won't exist
+export function validateAckByEvent([event, instance, callback], next) {
+  const validator = ackValidatorByEvent.get(event);
+  validateByValidator(validator, instance, next, callback);
+}
