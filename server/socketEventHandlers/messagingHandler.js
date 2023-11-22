889df1bda21bcd22cb1d6555fd0785190ca7a543
Matthew Snelgrove <matthew.s13542@gmail.com>
2023-11-21 21:20:18 -0500

    format messagingHandler, add getMessages note

--- server/socketEventHandlers/messagingHandler.js
+++ server/socketEventHandlers/messagingHandler.js
@@ -2,34 +2,26 @@ import { pool } from "../configServices/dbConfig.js";
 import camelize from "camelize";
 
 function getUid(socket) {
-  return socket.request.session.accountUid
+  return socket.request.session.accountUid;
 }
 
 function getMinMax(uid1, uid2) {
-  return [Math.max(uid1, uid2), Math.min(uid1, uid2)]
+  return [Math.max(uid1, uid2), Math.min(uid1, uid2)];
 }
 
 export default (io, socket) => {
+  // need to add seen field to messages based on sent time of message vs sent time of last read message
+  async function GetMessages(payload, callback) {}
 
-  async function GetMessages(payload, callback) {
-    
-  }
+  async function ReadMessages(payload, callback) {}
 
-  async function ReadMessages(payload, callback) {
+  async function SendMessage(payload, callback) {}
 
-  }
-
-  async function SendMessage(payload, callback) {
-
-  }
-
-  async function EditMessage(payload, callback) {
-    
-  }
+  async function EditMessage(payload, callback) {}
 
   async function DeleteMessage(payload, callback) {
-    const loggedUid = getUid(socket)
-    const messageUid = payload.messageUid
+    const loggedUid = getUid(socket);
+    const messageUid = payload.messageUid;
 
     const { actuallySent } = camelize(
       await pool.query(
@@ -43,17 +35,16 @@ export default (io, socket) => {
     }
 
     const { rows } = camelize(
-      await pool.query(
-        `DELETE * FROM message WHERE message_uid = $1`,
-        [messageUid]
-      )
+      await pool.query(`DELETE * FROM message WHERE message_uid = $1`, [
+        messageUid,
+      ])
     );
 
-    callback(rows)
+    callback(rows);
   }
-  
+
   async function GetConversations(payload, callback) {
-    const loggedUid = getUid(socket)
+    const loggedUid = getUid(socket);
 
     const { rows } = camelize(
       await pool.query(
@@ -62,32 +53,26 @@ export default (io, socket) => {
       )
     );
 
-    callback(rows)
+    callback(rows);
   }
 
   //I dont think I need any error checking stuff here
   //Also think its fine to trust messagerId because its only changing based off of getUid func
   async function LeaveConversation(payload, callback) {
-    const loggedUid = getUid(socket)
+    const loggedUid = getUid(socket);
 
-    const highUid = getMinMax(payload.messagerId, loggedUid)[0]
+    const highUid = getMinMax(payload.messagerId, loggedUid)[0];
 
-    const isHigh = loggedUid === highUid ? 'hi_active' : 'lo_active'
+    const isHigh = loggedUid === highUid ? "hi_active" : "lo_active";
 
     const { rows } = camelize(
-      await pool.query(
-        `UPDATE conversation SET $1 = false`,
-        [isHigh]
-      )
-    )
+      await pool.query(`UPDATE conversation SET $1 = false`, [isHigh])
+    );
 
-    callback(rows)
-
+    callback(rows);
   }
 
-  async function JoinConversation(payload, callback) {
-
-  }
+  async function JoinConversation(payload, callback) {}
 
   socket.on("GetMessages", GetMessages);
   socket.on("SendMessage", SendMessage);
