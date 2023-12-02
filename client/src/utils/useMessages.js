0e4a56f12be6fefdf9bfa05b28402fc1aad29740
Matthew Snelgrove <matthew.s13542@gmail.com>
2023-12-02 17:07:29 -0500

    remove debugging useEffect in useMessages
    Co-authored-by: zayleak <zayleak@users.noreply.github.com>

--- client/src/utils/useMessages.js
+++ client/src/utils/useMessages.js
@@ -13,43 +13,6 @@ const useMessages = (interlocutorUid, curMessageId = null, limit = 3) => {
   }
 
   useEffect(() => {
-    console.log(messages);
-    // console.log(conversations);
-
-    if (!socketRef.current) {
-      return;
-    }
-    // // if (!socketRef.current || conversations.length === 0 ) { return }
-
-    // // socketRef.current.emit("JoinConversation", {interlocutorUid: "9405b073-70ee-4a5d-a2bd-dbfc3709846c"}, (d) => {})
-    // // socketRef.current.emit("LeaveConversation", { interlocutorUid:  "9405b073-70ee-4a5d-a2bd-dbfc3709846c"}, (d) => {console.log(d)})
-
-    // socketRef.current.emit(
-    //   "sendMessage",
-    //   {
-    //     message: {
-    //       interlocutorUid: "9405b073-70ee-4a5d-a2bd-dbfc3709846c",
-    //       senderUid: "0913221f-e278-4e17-bfc4-436d7a5ee0c1",
-    //       content: "ooga booga",
-    //     },
-    //   },
-    //   (d) => {
-    //     console.log(d);
-    //   }
-    // );
-
-    socketRef.current.emit(
-      "deleteMessage",
-      {
-        messageUid: "5061b55b-a365-4894-a702-1d208f7b0e04",
-      },
-      (d) => {
-        console.log(d);
-      }
-    );
-  }, [messages]);
-
-  useEffect(() => {
     if (!user) {
       return;
     }
